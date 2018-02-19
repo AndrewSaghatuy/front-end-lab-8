22ae216d93f12f7bc9c32157ab38aea937cfd331
Andrew Saghatuy <json@i.ua>
2018-02-20 00:51:02 +0200

    Update isPrime

--- homework_08/js/isPrime.js
+++ homework_08/js/isPrime.js
@@ -3,8 +3,32 @@
  */
 function isPrime(number) {
     if(Number(number)) {
-        return number % 1 === 0;
-    } else {
+		
+	  var MaxNumber = 0;
+
+	  if (number%2 === 0 && number !== 2)
+	  {
+		return false;
+	  }
+	  
+	  if (number === 2)
+	  {
+		return true;
+	  }
+
+	  MaxNumber = Math.sqrt(number)
+
+	  for (var i = 3; i<MaxNumber; i+=2)
+	  {
+		if (number%i == 0)
+		{
+		  return false;
+		}
+	  }
+
+	  return true;
+	
+	} else {
         console.log('Incorrect data!');
     }
 }
